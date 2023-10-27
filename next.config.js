1a3223c7b9e18537cbc013497a012ec795dde2a6
Neander de Souza <neander.faria@hotmail.com>
2023-10-27 10:20:07 -0300

    Projeto de e-commerce - Página: Home

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['github.com'],
+    remotePatterns: [
+      {
+        // The `src` property hostname must end with `.example.com`,
+        // otherwise this will respond with 400 Bad Request.
+        // protocol: 'https',
+        // hostname: '**.github.com',
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
