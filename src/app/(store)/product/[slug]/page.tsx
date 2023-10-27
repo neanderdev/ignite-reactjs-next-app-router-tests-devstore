33492ad5bb7fa61672595dd34fd85898472021a3
Neander de Souza <neander.faria@hotmail.com>
2023-10-27 10:54:17 -0300

    Projeto de e-commerce - Adicionando ao carrinho

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
 
+import { AddToCartButton } from '@/components/add-to-cart-button'
+
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 
@@ -74,6 +76,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </span>
 
           <span className="text-sm text-zinc-400">
+            Em até 12x s/ juros de{' '}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
@@ -115,12 +118,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
