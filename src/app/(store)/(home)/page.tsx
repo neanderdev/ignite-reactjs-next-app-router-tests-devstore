afba60a56573240b96814f9cc7422e4c20e8ce15
Neander de Souza <neander.faria@hotmail.com>
2023-10-27 10:29:50 -0300

    Projeto de e-commerce - Cache & Memoization

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,8 +4,16 @@ import Link from 'next/link'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 
+/**
+ * Cache & Memoization
+ */
+
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
 
   const products = await response.json()
 
